2005504053e6fc3c662c4402230134610d69fd2f
Bao Long <baolongtp54@gmail.com>
2023-03-06 11:20:20 +0700

    Authorized, Login, Security

--- src/main/java/com/mockproject/MockProjectApplication.java
+++ src/main/java/com/mockproject/MockProjectApplication.java
@@ -1,9 +1,13 @@
 package com.mockproject;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "Authorization", scheme = "Bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class MockProjectApplication {
 
 	public static void main(String[] args) {
