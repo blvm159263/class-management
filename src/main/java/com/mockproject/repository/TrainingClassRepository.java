209b2a205106411cf9325cf5cfe631faa9742758
K <thekhoi.k@gmail.com>
2023-03-08 13:39:54 +0700

    complete merging

--- src/main/java/com/mockproject/repository/TrainingClassRepository.java
+++ src/main/java/com/mockproject/repository/TrainingClassRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface TrainingClassRepository extends JpaRepository<TrainingClass, Long>, JpaSpecificationExecutor<TrainingClass> {
-
+    public List<TrainingClass> findAllByListClassSchedulesDate(LocalDate date);
 }
