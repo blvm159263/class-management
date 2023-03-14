2b30d3e1798e7b1b0e62c509205bcfae4637efc4
K <thekhoi.k@gmail.com>
2023-03-14 13:54:23 +0700

    test merging

--- src/main/java/com/mockproject/repository/TrainingProgramRepository.java
+++ src/main/java/com/mockproject/repository/TrainingProgramRepository.java
@@ -1,31 +1,11 @@
 package com.mockproject.repository;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-import com.mockproject.dto.TrainingProgramDTO;
->>>>>>> origin/g3_hung_branch
-=======
-
->>>>>>> origin/g3_truong_branch
 import com.mockproject.entity.TrainingProgram;
-import com.mockproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TrainingProgramRepository extends JpaRepository<TrainingProgram, Long> {
-
-    TrainingProgram getTrainingProgramById(long id);
-
-    List<TrainingProgram> getTrainingProgramByNameContains(String name);
-
-    List<TrainingProgram> getAllByCreatorFullNameContains(String name);
-
-
-
 }
--- src/main/java/com/mockproject/service/TrainingProgramService.java
+++ src/main/java/com/mockproject/service/TrainingProgramService.java
@@ -1,63 +1,15 @@
 package com.mockproject.service;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/g3_hung_branch
-=======
-
->>>>>>> origin/g3_truong_branch
-import com.mockproject.entity.TrainingProgram;
-import com.mockproject.repository.TrainingProgramRepository;
 import com.mockproject.service.interfaces.ITrainingProgramService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import java.util.List;
->>>>>>> origin/g3_hung_branch
-=======
->>>>>>> origin/g3_truong_branch
 
 @Service
 @Transactional
 @AllArgsConstructor
-public class TrainingProgramService implements ITrainingProgramService{
-    private final TrainingProgramRepository trainingProgramRepository;
-    public List<TrainingProgram> getPrograms(){
-        return trainingProgramRepository.findAll();
-    }
+public class TrainingProgramService {
 
-
-    public TrainingProgram getTrainingProgramById(long id){
-        return repository.getTrainingProgramById(id);
-
-
-    @Override
-    public List<TrainingProgram> searchProgramP(String query) {
-        List<TrainingProgram> trainingPrograms = trainingProgramRepository.searchProgramP(query);
-        return trainingPrograms;
-    }
-
-    public List<TrainingProgram> getAll(Integer pageNo, Integer pageSize){
-        Pageable paging = PageRequest.of(pageNo, pageSize);
-        Page<TrainingProgram> result=repository.findAll(paging);
-        return result.getContent();
-    }
-    public List<TrainingProgram> getByName(String keyword){
-        return repository.getTrainingProgramByNameContains(keyword);
-    }
-    public long countAll(){
-        return repository.count();
-    }
-    public List<TrainingProgram> getByCreatorFullname(String keyword){
-        return repository.getAllByCreatorFullNameContains(keyword);
 
 }
