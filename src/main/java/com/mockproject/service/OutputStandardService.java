29bb783e18c055b9f83eb9cde3a7298bdeac4753
tam <tamminhle2002@gmail.com>
2023-03-15 09:54:23 +0700

    Get all OutputStandard

--- src/main/java/com/mockproject/service/OutputStandardService.java
+++ src/main/java/com/mockproject/service/OutputStandardService.java
@@ -1,10 +1,14 @@
 package com.mockproject.service;
 
+import com.mockproject.dto.DeliveryTypeDTO;
 import com.mockproject.dto.OutputStandardDTO;
+import com.mockproject.entity.DeliveryType;
 import com.mockproject.entity.OutputStandard;
+import com.mockproject.mapper.DeliveryTypeMapper;
 import com.mockproject.mapper.OutputStandardMapper;
 import com.mockproject.repository.OutputStandardRepository;
 import com.mockproject.service.interfaces.IOutputStandardService;
+import com.mockproject.utils.ListUtils;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,4 +35,13 @@ public class OutputStandardService implements IOutputStandardService {
         return outputStandardDTO;
     }
 
+    public List<OutputStandardDTO> getOutputStandard(boolean status) {
+        Optional<List<OutputStandard>> outputStandardList = outputStandardRepository.findByStatus(status);
+        ListUtils.checkList(outputStandardList);
+        List<OutputStandardDTO> outputStandardDTOList = new ArrayList<>();
+        for (OutputStandard o: outputStandardList.get()){
+            outputStandardDTOList.add(OutputStandardMapper.INSTANCE.toDTO(o));
+        }
+        return outputStandardDTOList;
+    }
 }
