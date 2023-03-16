8511cad2028c863c979c6be217d1d7ada8c7b86f
Bao Long <baolongtp54@gmail.com>
2023-03-16 13:23:30 +0700

    Custom UI

--- src/main/java/com/mockproject/controller/DeliveryTypeController.java
+++ src/main/java/com/mockproject/controller/DeliveryTypeController.java
@@ -2,7 +2,10 @@ package com.mockproject.controller;
 
 import com.mockproject.dto.DeliveryTypeDTO;
 import com.mockproject.service.interfaces.IDeliveryTypeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/delivery")
-
+@Tag(name = "Delivery type", description = "API related delivery type")
 @RequiredArgsConstructor
 public class DeliveryTypeController {
 
@@ -25,12 +28,14 @@ public class DeliveryTypeController {
     private final IDeliveryTypeService deliveryTypeService;
 
     @GetMapping("")
+    @Operation(summary = "Get all delivery type")
     public ResponseEntity<List<DeliveryTypeDTO>> getAll(){
         List<DeliveryTypeDTO> deliveryTypeDTOList = deliveryTypeService.getDeliveryTypes(true);
         return ResponseEntity.ok(deliveryTypeDTOList);
     }
 
     @GetMapping("/{deliveryTypeId}")
+    @Operation(summary = "get Delivery type by delivery type id")
     public ResponseEntity<DeliveryTypeDTO> getDeliveryTypeById(@PathVariable("deliveryTypeId") long id){
         DeliveryTypeDTO deliveryTypeDTO = deliveryTypeService.getDeliveryType(id, true);
         return ResponseEntity.ok(deliveryTypeDTO);
--- src/main/java/com/mockproject/controller/OutputStandardController.java
+++ src/main/java/com/mockproject/controller/OutputStandardController.java
@@ -2,7 +2,9 @@ package com.mockproject.controller;
 
 import com.mockproject.dto.OutputStandardDTO;
 import com.mockproject.service.interfaces.IOutputStandardService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Output Standard", description = "API related Output standard")
 @RequestMapping(value = "/api/outputStandard")
 public class OutputStandardController {
 
@@ -22,12 +25,14 @@ public class OutputStandardController {
     private final IOutputStandardService outputStandardService;
 
     @GetMapping("/{outputStandardId}")
+    @Operation(summary = "Get output standard by output standard id")
     public ResponseEntity<OutputStandardDTO> getOutputStandardById(@PathVariable("outputStandardId") long id){
         OutputStandardDTO outputStandardDTO = outputStandardService.getOutputStandardById(id, true);
         return ResponseEntity.ok(outputStandardDTO);
     }
 
     @GetMapping("")
+    @Operation(summary = "Get all output standard")
     public ResponseEntity<List<OutputStandardDTO>> getAll(){
         List<OutputStandardDTO> outputStandardDTOList = outputStandardService.getOutputStandard(true);
         return ResponseEntity.ok(outputStandardDTOList);
