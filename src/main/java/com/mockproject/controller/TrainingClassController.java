d9f56b0026537c1948e09203544573403dc4df9f
thinhpham2k2 <thinhphamquoc9999@gmail.com>
2023-03-20 10:37:24 +0700

    Merge g2_branch to dev

--- src/main/java/com/mockproject/controller/TrainingClassController.java
+++ src/main/java/com/mockproject/controller/TrainingClassController.java
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.repository.query.Param;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
