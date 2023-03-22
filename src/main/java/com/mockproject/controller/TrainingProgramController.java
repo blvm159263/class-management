e9a52d99b3f3bd681e8a79b9998a3d3f5e8a55ba
THUC NGUYEN <ntthuc321@gmail.com>
2023-03-22 10:23:10 +0700

    aa

--- src/main/java/com/mockproject/controller/TrainingProgramController.java
+++ src/main/java/com/mockproject/controller/TrainingProgramController.java
@@ -1,9 +1,16 @@
 package com.mockproject.controller;
 
+import com.mockproject.dto.ReadFileDto;
 import com.mockproject.dto.TrainingProgramDTO;
+import com.mockproject.entity.TrainingProgram;
+import com.mockproject.exception.FileException;
+import com.mockproject.service.interfaces.IFileService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.ui.Model;
 import com.mockproject.service.interfaces.ITrainingProgramService;
@@ -18,14 +25,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -33,6 +47,7 @@ import java.util.List;
 public class TrainingProgramController {
 
     private final ITrainingProgramService service;
+    private final IFileService fileService;
 
     @ApiResponses(value = {
             @ApiResponse(responseCode = "404", description = "When don't find any Training Program"),
@@ -125,6 +140,70 @@ public class TrainingProgramController {
             session.removeAttribute("LIST_NAME");
         }
         session.setAttribute("LIST_NAME", listName);
-        return ResponseEntity.ok().body("detele successfully");
+        return ResponseEntity.ok().body("Delete successfully");
+    }
+    @PostMapping("/uploadCsv")
+    public ResponseEntity readFileCsv(@ModelAttribute ReadFileDto readFileDto){
+        MultipartFile file = readFileDto.getFile();
+        List<TrainingProgram> trainingProgramList= new ArrayList<>();
+        if(file.isEmpty()){
+            return ResponseEntity.badRequest().body("File is empty");
+        }else{
+            String fileName= file.getOriginalFilename();
+            try{
+                if(!fileName.split("\\.")[1].equals("csv")){
+                    return ResponseEntity.badRequest().body("File is not csv");
+                }else{
+                    //CustomUserDetails user = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                    Long creatorId,lastModifierId;
+                    int programId;
+                    LocalDate dateCreated, lastDateModified;
+                    String name;
+                    BigDecimal hour;
+                    int day;
+                    boolean status;
+                    List<Long> listTrainingClassesId,listTrainingProgramSyllabusesId;
+                   // BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+
+//                    CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT.builder().setSkipHeaderRecord(true).build());
+
+                    CSVParser parser= fileService.readFile(file,readFileDto.getEncodingType());
+                    // skip header of csv file
+                    parser.iterator().next();
+                    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                    for (CSVRecord record : parser.getRecords()){
+                        try{
+                            dateCreated= LocalDate.parse(record.get(2),dateFormat);
+                            lastDateModified = LocalDate.parse(record.get(3),dateFormat);
+                        }catch (DateTimeException e){
+                            throw new FileException("Date time is wrong( format: yyyy-MM-dd)", HttpStatus.BAD_REQUEST.value());
+                        }
+                        try{
+                            programId = Integer.parseInt(record.get(0));
+                            name = record.get(1);
+                            hour = new BigDecimal(record.get(4));
+                            day = Integer.parseInt(record.get(5));
+                            status = Boolean.parseBoolean(record.get(6));
+                            creatorId = Long.parseLong(record.get(7));
+                            lastModifierId = Long.parseLong(record.get(8));
+                            listTrainingClassesId = Arrays.stream(record.get(9).split("/"))
+                                    .map(classId-> Long.parseLong(classId))
+                                    .collect(Collectors.toList());
+                            listTrainingProgramSyllabusesId = Arrays.stream(record.get(10).split("/"))
+                                    .map(classId-> Long.parseLong(classId))
+                                    .collect(Collectors.toList());
+                        }catch (Exception e){
+                            throw new FileException (e.getMessage(),HttpStatus.BAD_REQUEST.value());
+                        }
+                        var trainingProgram = new TrainingProgram(null,programId,name,dateCreated,lastDateModified,hour,day,status,null,null,null,null);
+//                        System.out.println(trainingProgram.toString());
+//                        System.out.println(creatorId+" "+lastModifierId+" " +listTrainingClassesId+" "+listTrainingProgramSyllabusesId);
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return ResponseEntity.ok().body("Okeee");
     }
 }
---
+++ src/main/java/com/mockproject/service/interfaces/IFileService.java
@@ -0,0 +1,18 @@
+package com.mockproject.service.interfaces;
+
+import com.mockproject.exception.FileException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public interface   IFileService {
+    public CSVParser readFile(MultipartFile file, String encodingType);
+
+}
