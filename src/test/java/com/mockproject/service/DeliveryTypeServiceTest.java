093c66a3efd1b3fd8c8fe41fc06b7ff79815d1e3
vinhquangle <haclong0512@gmail.com>
2023-03-23 18:55:18 +0700

    Update DeliveryTypeServiceTest

--- src/test/java/com/mockproject/service/DeliveryTypeServiceTest.java
+++ src/test/java/com/mockproject/service/DeliveryTypeServiceTest.java
@@ -1,12 +1,27 @@
 package com.mockproject.service;
 
-import com.mockproject.entity.DeliveryType;
+import com.mockproject.dto.DeliveryTypeDTO;
+import com.mockproject.entity.*;
 import com.mockproject.repository.DeliveryTypeRepository;
+import com.mockproject.repository.TrainingClassRepository;
+import com.mockproject.repository.TrainingClassUnitInformationRepository;
+import com.mockproject.repository.UnitDetailRepository;
+import com.mockproject.service.interfaces.IUnitService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ContextConfiguration(classes = {DeliveryTypeService.class})
 @ExtendWith(SpringExtension.class)
@@ -14,12 +29,41 @@ class DeliveryTypeServiceTest {
     @MockBean
     private DeliveryTypeRepository deliveryTypeRepository;
 
+    @MockBean
+    private UnitDetailRepository unitDetailRepository;
+
+    @MockBean
+    private IUnitService iUnitService;
+
+    @MockBean
+    private TrainingClassRepository trainingClassRepository;
+
+    @MockBean
+    private TrainingClassUnitInformationRepository trainingClassUnitInformationRepository;
+
     @Autowired
     private DeliveryTypeService deliveryTypeService;
 
     DeliveryType deliveryType1 = new DeliveryType(1L, "On Meeting", true, null);
-    DeliveryType deliveryType2 = new DeliveryType(2L, "Offline", false, null);
+    DeliveryType deliveryType2 = new DeliveryType(2L, "Offline", true, null);
     DeliveryType deliveryType3 = new DeliveryType(3L, "Practice", true, null);
+
+    TrainingClass tc1 = new TrainingClass(1L, "Class Name 1", "TC1", null, null,
+            null, null, 12, 30, 30, 25, "Planning", null,
+            null, null, null, 1, true, null, null,
+            null, null, null, null, null, null, null,
+            null, null, null);
+
+    Unit unit1 = new Unit(1L, "Unit title 123", 1, BigDecimal.TEN, true, null,
+            null, null);
+    Unit unit2 = new Unit(2L, "Unit title 234", 2, BigDecimal.TEN, true, null,
+            null, null);
+
+    UnitDetail unitDetails1 = new UnitDetail(1L, "Unit details title 1",  BigDecimal.TEN, true, true,
+            unit1, deliveryType1, null, null);
+    UnitDetail unitDetails2 = new UnitDetail(2L, "Unit details title 2",  BigDecimal.TEN, true, true,
+            unit2, deliveryType2, null, null);
+
 
     /**
      * Method under test: {@link DeliveryTypeService#getByIdTrue(Long)}
@@ -37,5 +81,32 @@ class DeliveryTypeServiceTest {
 //        assertEquals("Practice", result.getTypeName());
 //        verify(deliveryTypeRepository).findByIdAndStatus(3L, true);
 //    }
+
+    /**
+     * Method under test: {@link DeliveryTypeService#getAllDeliveryTypesByTrainingClassId(Long)}
+     */
+    @Test
+    void canGetAllDeliveryTypesByTrainingClassId() {
+        Long trainingClassId = 1L;
+
+        List<Unit> unitList = new ArrayList<>();
+        unitList.add(unit1);
+        unitList.add(unit2);
+
+        List<UnitDetail> unitDetailList = new ArrayList<>();
+        unitDetailList.add(unitDetails1);
+        unitDetailList.add(unitDetails2);
+
+        when(iUnitService.getListUnitsByTrainingClassId(trainingClassId)).thenReturn(unitList);
+        when(unitDetailRepository.findByUnitInAndStatus(unitList,true)).thenReturn(Optional.of(unitDetailList));
+
+        List<DeliveryTypeDTO> deliveryType = deliveryTypeService.getAllDeliveryTypesByTrainingClassId(tc1.getId());
+        assertEquals(1L, deliveryType.get(0).getId());
+        assertEquals("On Meeting", deliveryType.get(0).getTypeName());
+
+        verify(iUnitService).getListUnitsByTrainingClassId(trainingClassId);
+        verify(unitDetailRepository).findByUnitInAndStatus(unitList, true);
+    }
+
 }
 
