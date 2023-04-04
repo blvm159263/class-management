ab89aa3fbd06c87945f00b55b36944ccc4d7bcc6
thinhpham2k2 <thinhphamquoc9999@gmail.com>
2023-04-04 16:47:10 +0700

    Update syllabus api

--- src/main/java/com/mockproject/service/OutputStandardService.java
+++ src/main/java/com/mockproject/service/OutputStandardService.java
@@ -39,10 +39,10 @@ public class OutputStandardService implements IOutputStandardService {
 
     @Override
     public List<OutputStandardDTO> getOsdBySyllabusId(boolean status, Long id) {
-        List<OutputStandard> osd = unitDetailRepo.findUnitDetailBySyllabusId(status, id)
-                .stream().distinct().collect(Collectors.toList());
+        List<OutputStandardDTO> osd = unitDetailRepo.findUnitDetailBySyllabusId(status, id)
+                .stream().map(OutputStandardMapper.INSTANCE::toDTO).distinct().collect(Collectors.toList());
         if(osd.size() > 0){
-            return osd.stream().map(OutputStandardMapper.INSTANCE::toDTO).collect(Collectors.toList());
+            return osd;
         }else {
             throw new NotFoundException("Output standard not found with syllabus id: " + id);
         }
